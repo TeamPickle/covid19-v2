d2bb7adcd79bacab6451378c30de71ef28cb2cf5
CraftyDragon678 <craftydragon678@gmail.com>
2020-08-29 07:31:22 +0900

    feat: check admin decorator

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,2 @@
 from .mode import get_mode, Mode
+from .permission import checkadmin
---
+++ utils/permission.py
@@ -0,0 +1,15 @@
+from discord.ext.commands import Context
+from bot import CovidBot
+
+def checkadmin(super_admin=False):
+    def decorator(func):
+        async def wrapper(self, ctx: Context, *args):
+            bot: CovidBot = ctx.bot
+            if super_admin:
+                if bot.pickle_db.pickle.superadmins.find_one({"_id": ctx.author.id}):
+                    await func(self, ctx, *args)
+            else:
+                if bot.pickle_db.pickle.admins.find_one({"_id": ctx.author.id}):
+                    await func(self, ctx, *args)
+        return wrapper
+    return decorator
