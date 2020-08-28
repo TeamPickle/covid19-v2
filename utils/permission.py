a2dcde1e9ddf331116fa600e925448314a459366
CraftyDragon678 <craftydragon678@gmail.com>
2020-08-29 07:39:53 +0900

    fix: string filter vars

--- utils/permission.py
+++ utils/permission.py
@@ -6,10 +6,10 @@ def checkadmin(super_admin=False):
         async def wrapper(self, ctx: Context, *args):
             bot: CovidBot = ctx.bot
             if super_admin:
-                if bot.pickle_db.pickle.superadmins.find_one({"_id": ctx.author.id}):
+                if bot.pickle_db.pickle.superadmins.find_one({"_id": str(ctx.author.id)}):
                     await func(self, ctx, *args)
             else:
-                if bot.pickle_db.pickle.admins.find_one({"_id": ctx.author.id}):
+                if bot.pickle_db.pickle.admins.find_one({"_id": str(ctx.author.id)}):
                     await func(self, ctx, *args)
         return wrapper
     return decorator
