f60817c4a1cef0766908ef453d2812cbffc4f96f
CraftyDragon678 <craftydragon678@gmail.com>
2020-08-29 16:44:49 +0900

    refactor: import

--- cogs/status.py
+++ cogs/status.py
@@ -1,10 +1,7 @@
 from discord import Embed, Forbidden
 from discord.ext.commands import Cog, Context, command
 from bot import CovidBot
-import aiohttp
-import re
-import math
-import asyncio
+import aiohttp, re, math, asyncio
 
 _DISASTER_REGION = ["강원", "경기", "경남", "경북", "광주", "대구", "대전",
                    "부산", "서울", "울산", "인천", "전남", "전북", "제주", "충남", "충북", "세종"]
