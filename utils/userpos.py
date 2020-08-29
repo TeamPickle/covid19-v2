fefc4519bec8a5b4f317196745fb1358fe5f2ea1
CraftyDragon678 <craftydragon678@gmail.com>
2020-08-29 14:23:32 +0900

    fix(userpos): check if args is empty

--- utils/userpos.py
+++ utils/userpos.py
@@ -4,7 +4,7 @@ from bot import CovidBot
 def userpos(func):
     async def wrapper(self, ctx: Context, *args):
         bot: CovidBot = ctx.bot
-        if result := bot.pickle_db["covid19"]["userloc"].find_one({"_id": ctx.author.id}):
+        if len(args) == 0 and (result := bot.pickle_db["covid19"]["userloc"].find_one({"_id": ctx.author.id})):
             args = result["loc"].split(" ")
         await func(self, ctx, *args)
     return wrapper
