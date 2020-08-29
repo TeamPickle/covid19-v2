bda88b2b5a1326b9d06f6d55fc2d00e1cdaba11f
CraftyDragon678 <craftydragon678@gmail.com>
2020-08-29 11:26:13 +0900

    feat: update check permission decorator

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,2 @@
 from .mode import get_mode, Mode, server_command
-from .permission import checkadmin, serveradmin
+from .permission import checkadmin, guild_permission
--- utils/permission.py
+++ utils/permission.py
@@ -15,11 +15,11 @@ def checkadmin(super_admin=False):
         return wrapper
     return decorator
 
-def serveradmin():
+def guild_permission(idx: int=3):
     def decorator(func):
         async def wrapper(self, ctx: Context, *args):
             author: Member = ctx.author
-            print(author.guild_permissions.value >> 3 & 1)
-            await func(self, ctx, *args)
+            if author.guild_permissions.value >> (idx - 1) & 1:
+                await func(self, ctx, *args)
         return wrapper
     return decorator
