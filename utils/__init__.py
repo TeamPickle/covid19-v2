dca452cc31615e16c49d634e36707e88f46b71c9
CraftyDragon678 <craftydragon678@gmail.com>
2020-08-29 14:16:46 +0900

    feat: add userpos decorator

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,3 @@
 from .mode import get_mode, Mode, server_command, dm_command
 from .permission import checkadmin, guild_permission
+from .userpos import userpos
---
+++ utils/userpos.py
@@ -0,0 +1,10 @@
+from discord.ext.commands import Context
+from bot import CovidBot
+
+def userpos(func):
+    async def wrapper(self, ctx: Context, *args):
+        bot: CovidBot = ctx.bot
+        if result := bot.pickle_db["covid19"]["userloc"].find_one({"_id": ctx.author.id}):
+            args = result["loc"].split(" ")
+        await func(self, ctx, *args)
+    return wrapper
