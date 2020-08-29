861ea6c6b9cf2e1acf3c2a86c1b6e93108d54923
CraftyDragon678 <craftydragon678@gmail.com>
2020-08-29 13:51:25 +0900

    feat: noti, dnd command

--- cogs/channel.py
+++ cogs/channel.py
@@ -40,6 +40,37 @@ class Channel(Cog):
                 return
         await ctx.send(f"명령어 사용법 : ``{ctx.prefix}채널설정 #공지``")
 
+    @command(name="noti", aliases=["중요뉴스"])
+    @utils.server_command
+    @utils.guild_permission("서버관리자만 중요뉴스 받기 옵션을 지정할 수 있습니다.")
+    async def noti(self, ctx: Context, *args):
+        if args:
+            guild: Guild = ctx.guild
+            if args[0] == "ㅇ":
+                self.db["covid19"]["noti"].insert_one({"_id": guild.id})
+                await ctx.send("중요뉴스만 받기 옵션이 설정되었습니다.")
+                return
+            elif args[0] == "ㄴ":
+                self.db["covid19"]["noti"].remove({"_id": guild.id})
+                await ctx.send("중요뉴스만 받기 옵션이 해제되었습니다.")
+                return
+        await ctx.send(f"명령어 사용법 : ``{ctx.prefix}중요뉴스 [ㅇ/ㄴ]``")
+
+    @command(name="dnd", aliases=["방해금지"])
+    @utils.server_command
+    @utils.guild_permission("서버관리자만 방해금지 모드 옵션을 지정할 수 있습니다.")
+    async def dnd(self, ctx: Context, *args):
+        if args:
+            guild: Guild = ctx.guild
+            if args[0] == "ㅇ":
+                self.db["covid19"]["dnd"].insert_one({"_id": guild.id})
+                await ctx.send("방해금지 모드가 설정되었습니다.")
+                return
+            elif args[0] == "ㄴ":
+                self.db["covid19"]["dnd"].remove({"_id": guild.id})
+                await ctx.send("방해금지 모드가 해제되었습니다.")
+                return
+        await ctx.send(f"명령어 사용법 : ``{ctx.prefix}방해금지 [ㅇ/ㄴ]``")
 
 def setup(bot: CovidBot):
     bot.add_cog(Channel(bot))
