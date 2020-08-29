bd1a2db24741cc107bda32f20bec0de5a36c7a17
CraftyDragon678 <craftydragon678@gmail.com>
2020-08-29 11:45:33 +0900

    refactor: get database, collection like dict

--- utils/permission.py
+++ utils/permission.py
@@ -7,10 +7,10 @@ def checkadmin(super_admin=False):
         async def wrapper(self, ctx: Context, *args):
             bot: CovidBot = ctx.bot
             if super_admin:
-                if bot.pickle_db.pickle.superadmins.find_one({"_id": str(ctx.author.id)}):
+                if bot.pickle_db["pickle"]["superadmins"]find_one({"_id": str(ctx.author.id)}):
                     await func(self, ctx, *args)
             else:
-                if bot.pickle_db.pickle.admins.find_one({"_id": str(ctx.author.id)}):
+                if bot.pickle_db["pickle"]["admins"]find_one({"_id": str(ctx.author.id)}):
                     await func(self, ctx, *args)
         return wrapper
     return decorator
