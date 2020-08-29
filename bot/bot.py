4466b429fcd13bebdbd8937312e8381bbd56cff5
CraftyDragon678 <craftydragon678@gmail.com>
2020-08-29 09:01:19 +0900

    feat: error logging

--- bot/bot.py
+++ bot/bot.py
@@ -1,7 +1,10 @@
-from discord import Game, Message
-from discord.ext.commands import AutoShardedBot
+from discord import Game, Message, Embed
+from discord.ext.commands import AutoShardedBot, Context
+from discord.ext.commands.errors import CommandNotFound
 import logging
 import os
+import random
+import traceback
 from db import PickleDB
 
 class CovidBot(AutoShardedBot):
@@ -38,6 +41,34 @@ class CovidBot(AutoShardedBot):
             activity=Game("!도움으로 명령어 확인")
         )
 
+    async def on_command_error(self, ctx: Context, e: Exception):
+        if isinstance(e, CommandNotFound):
+            await super().on_command_error(ctx, e)
+            return
+        logChannel = os.getenv("LOG_CHANNEL")
+        if logChannel:
+            t = "%x" % random.randint(0, 16**8)
+            embed = Embed(
+                title="⚠️ 오류가 발생했습니다.",
+                description="[팀 피클 공식 포럼](http://forum.tpk.kr)에 버그를 제보해주세요.\n" \
+                    f"코드 ``{t}``를 알려주시면 더 빠른 처리가 가능합니다.",
+                color=0xffff00
+            )
+            await ctx.send(embed=embed)
+            await (await self.fetch_channel(logChannel)).send(
+                ("Code: {t}\n" \
+                    "Requester: {requesterStr}#{requesterS}({requesterNum})\n" \
+                    "Message: {message}\n" \
+                    "```py\n{trace}```").format(
+                        t=t,
+                        requesterStr=ctx.author.name,
+                        requesterS=ctx.author.discriminator,
+                        requesterNum=ctx.author.id,
+                        message=ctx.message.content,
+                        trace="".join(traceback.format_exception(type(e), e, e.__traceback__))
+                )
+            )
+        
 
 def get_command_prefix(bot: CovidBot, msg: Message):
-    return os.getenv("PREFIX") or "!"
+    return os.getenv("prefix") or "!"
