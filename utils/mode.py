2ab135672fc9873a97d05195d733b0b566bfd504
CraftyDragon678 <craftydragon678@gmail.com>
2020-12-22 17:04:18 +0900

    fix: get mode with guild

--- utils/mode.py
+++ utils/mode.py
@@ -6,16 +6,11 @@ from functools import wraps
 class Mode(Enum):
     SERVER = 0
     DM = 1
-    UNKNOWN = 2
 
 def get_mode(ctx: Context) -> Mode:
-    if type(ctx.channel) == TextChannel:
-        mode = Mode.SERVER
-    elif type(ctx.channel) == DMChannel:
-        mode = Mode.DM
-    else:
-        mode = Mode.UNKNOWN
-    return mode
+    if ctx.guild is None:
+        return Mode.DM
+    return Mode.SERVER
 
 def server_command(func):
     @wraps(func)
